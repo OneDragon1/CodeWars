7c8105a7abd05d3508ced5e69d08bab4aa8d5acb
OneDragon1 <92191539+OneDragon1@users.noreply.github.com>
2022-06-28 19:00:49 -0700

    Add files via upload

---
+++ evenOrOdd.js
@@ -0,0 +1,10 @@
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+
+
+//solution
+
+function even_or_odd(number) {
+    return number % 2 ? "Odd" : "Even"
+  }
