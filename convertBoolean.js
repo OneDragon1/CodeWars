c33cb4f6cce870680a2880381be41f07e3e9aa4a
OneDragon1 <92191539+OneDragon1@users.noreply.github.com>
2022-07-06 18:33:58 -0700

    Add files via upload

---
+++ convertBoolean.js
@@ -0,0 +1,16 @@
+
+//Challenge//
+//Convert boolean values to strings 'Yes' or 'No'.
+
+
+
+
+//solution//
+
+function boolToWord( bool ){
+    if(bool === true){
+      return "Yes"
+    }else{
+      return "No"
+    }
+  }
