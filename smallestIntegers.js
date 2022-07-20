f8eed67c9795744db418a8cd71aeb722b40d60a6
OneDragon1 <92191539+OneDragon1@users.noreply.github.com>
2022-07-20 13:31:39 -0700

    Add files via upload

---
+++ smallestIntegers.js
@@ -0,0 +1,11 @@
+//Challenge
+///Given an array of integers your solution should find the smallest integer.
+
+
+
+
+///solution
+
+class SmallestIntegerFinder {
+    findSmallestInt = args => Math.min(...args)
+  }
