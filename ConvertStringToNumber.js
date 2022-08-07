89d3a2e9672988b01bcdafeac764fbea17c05ab8
OneDragon1 <92191539+OneDragon1@users.noreply.github.com>
2022-08-07 09:37:24 -0700

    Add files via upload

---
+++ ConvertStringToNumber.js
@@ -0,0 +1,10 @@
+///We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+
+
+//solution//
+
+const stringToNumber = function(str){
+    const convert = str/1;
+    return convert;
+  }
