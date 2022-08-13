1b394fe559cdb67e7cfb422fc502d3cce64e1841
Gonzalo Mondragon <92191539+OneDragon1@users.noreply.github.com>
2022-08-12 18:26:40 -0700

    Add files via upload

---
+++ ConvertBooleantoString.js
@@ -0,0 +1,12 @@
+///instructions
+///Implement a function which convert the given boolean value into its string representation
+
+
+
+
+
+
+///solution
+function booleanToString(b){
+    return b+'';
+  }
