35d1719f646e4b56382aca8d96cea3ef2b60bdea
Gonzalo Mondragon <92191539+OneDragon1@users.noreply.github.com>
2022-08-24 17:01:49 -0700

    Add files via upload

---
+++ YouOnlyNeedOne.js
@@ -0,0 +1,9 @@
+//instructions
+//You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+//solution
+
+function check(a, x) {
+    // your code here
+    return a.includes(x);
+  }
