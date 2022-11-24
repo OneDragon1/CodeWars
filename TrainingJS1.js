0134f7ed47152f126975fcaf793585eeca3c0337
Gonzalo Mondragon <92191539+OneDragon1@users.noreply.github.com>
2022-11-24 09:34:49 -0800

    Add files via upload

---
+++ TrainingJS1.js
@@ -0,0 +1,11 @@
+/*Training JS #1: create your first JS function and print "Hello World!" */
+
+
+/* solution */
+
+
+function helloWorld(){
+    let someword = "Hello World!"
+    
+    console.log(someword)
+  }
