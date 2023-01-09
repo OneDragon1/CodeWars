eb63ea789ccf6520614d1c37ab9c7a8c4432a541
Gonzalo Mondragon <92191539+OneDragon1@users.noreply.github.com>
2023-01-08 20:55:05 -0800

    Add files via upload

---
+++ filterOutthe.js
@@ -0,0 +1,14 @@
+/**Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed. */
+
+
+
+//solution//
+
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    
+    return birds.filter(function(bird) {
+      return !geese.includes(bird);
+    });
+  }
